740ddc223943fb7158edb34310a98332b0e9d6f4
Brigitte Charles <brigitte.charles@Digital-Grenoble.local>
2019-02-26 13:49:51 +0100

    New text hello word

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello 
+                    Hello Word
                 </div>
 
                 <div class="links">
