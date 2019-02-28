ebdf6813b4d675e31a1f3706554857c81c5fd3a6
Brigitte Charles <brigitte.charles@Digital-Grenoble.local>
2019-02-28 15:04:26 +0100

    Mise à jour style CSS & Remplissage des pages CGV / Mentions légales et contact

--- resources/views/general_pages/mentions.blade.php
+++ resources/views/general_pages/mentions.blade.php
@@ -1,5 +1,22 @@
 @extends('simple-page')
-@section('title', 'Nom entreprise Conditions Générales de Ventes')
+@section('title', 'Nom entreprise Mentions légales')
 @section('content')
-    <h1>mentions légales</h1>
+    <section class="bande">
+        <div class="container">
+            <h1 class="pages">Mentions légales</h1>
+            <p>L'information précontractuelle est organisée par l'article L. 442-6 I 9° du Code de commerce qui fait obligation à tout producteur,
+                commerçant, industriel ou personne immatriculée au répertoire des métiers de communiquer ses conditions générales de vente à tout acheteur
+                de produits ou tout demandeur de prestations de services qui en fait la demande pour l'exercice d'une activité professionnelle. </p>
+            <p> Cette communication s'effectue par tout moyen conforme aux usages de la profession.</p>
+
+            <p>Les conditions générales de vente peuvent être différenciées selon les catégories d'acheteurs de produits ou de demandeurs de prestations
+                de services (par exemple, détaillants, grossistes). Dans ce cas, l'obligation de communication ne s'applique qu'à l'égard des acheteurs de produits
+                ou des demandeurs de prestation de services d'une même catégorie. Les conditions générales de vente constituant le socle unique de la négociation commerciale,
+                tout producteur, prestataire de services, grossiste ou importateur peut, par ailleurs, dans le cadre de cette négociation convenir avec un acheteur de produits
+                ou un demandeur de prestation de services des conditions particulières de vente qui ne sont pas soumises à cette obligation de communication.</p>
+
+        </div>
+    </section>
+    <section>
+    </section>
 @endsection
