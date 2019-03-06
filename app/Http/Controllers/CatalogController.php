f42c5d388e3bc9d03ae7a26b3cead91e09308272
YassineSeghir <linkovitchc@hotmail.com>
2019-03-06 15:12:11 +0100

    catalogue trier par prix desc

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -11,7 +11,7 @@ class CatalogController extends Controller
 
     public function showCatalog()
     {
-        $products = app\Product::all()->sortBy('name');
+        $products = app\Product::orderBy('price' , 'desc')->get();
 
 
         return view('catalog', ["product" => $products]);
