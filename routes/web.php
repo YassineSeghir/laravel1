ea4110614fde163cf6fd18c8fe1bba56ad394bbd
Richard Weber <richard.weber@Digital-Grenoble.local>
2019-03-20 10:26:12 +0100

    cleaned routes regarding product frontoffice

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,41 @@
 <?php
 
 Route::get('/', 'PagesController@showHome')
-    ->name('contact');
+    ->name('home');
 Route::get('/pages/cgv', 'PagesController@showCGV'
 )->name('cgv');
 Route::get('/pages/mentions', 'PagesController@showMentions')
     ->name('mentions');
-Route::get('/pages/contact', 'PagesController@showContact');
+Route::get('/pages/contact', 'PagesController@showContact')
+    ->name('home');
 
+
+Route::get('/catalog', 'ProductController@index')
+    ->name('catalog');
+Route::post('/product', 'ProductController@store')
+    ->name('basketStore');
+Route::get('/product/{id}', 'ProductController@show')
+    ->name('prod');
 
 
 Route::get('panier', 'BasketController@showPanier')
     ->name('basket');
 
+//Route::get('/customer', 'CustomerController@index');
+//Route::get('/customer/create', 'CustomerController@create');
+//Route::post('/customer', 'CustomerController@store');
+//Route::get('/customer/{id}', 'CustomerController@show');
+//Route::get('/customer/{id}/edit', 'CustomerController@edit');
+//Route::put('/customer/{id}', 'CustomerController@update');
+//Route::delete('/customer/{id}', 'CustomerController@destroy');
+
+
+//*********************************************************************************************************************
+//********BACKOFFICE***************************************************************************************************
+//*********************************************************************************************************************
 Route::get('admin/login', 'SuperadminController@index')
     ->name('admin');
+
 Route::get('/admin/administration', 'CatalogController@showCatalog')
     ->name('back');
 Route::get('admin/productcreate', 'SuperadminController@create')
@@ -30,24 +51,17 @@ Route::post('admin/productdelete', 'SuperadminController@delete')
     ->name('delete');
 
 
-Route::get('/catalog', 'ProductController@index')
-    ->name('catalog');
-Route::get('/product/create', 'ProductController@create');
-Route::post('/product', 'ProductController@store');
-Route::get('/product/{id}', 'ProductController@show')
-    ->name('prod');
-Route::get('/product/{id}/edit', 'ProductController@edit');
-Route::put('/product/{id}', 'ProductController@update');
-Route::delete('/product/{id}', 'ProductController@destroy');
-
-
-Route::get('/customer', 'CustomerController@index');
-Route::get('/customer/create', 'CustomerController@create');
-Route::post('/customer', 'CustomerController@store');
-Route::get('/customer/{id}', 'CustomerController@show');
-Route::get('/customer/{id}/edit', 'CustomerController@edit');
-Route::put('/customer/{id}', 'CustomerController@update');
-Route::delete('/customer/{id}', 'CustomerController@destroy');
-
-
-
+Route::get('/admin/catalog', 'AdminProductController@index')
+    ->name('admin_productCatalog');
+Route::get('/admin/product/create', 'AdminProductController@create')
+    ->name('admin_productCreate');
+Route::post('/admin/product/created', 'AdminProductController@store')
+    ->name('admin_productCreated');
+Route::get('/admin/product/{id}', 'AdminProductController@show')
+    ->name('admin_productShow');
+Route::get('/admin/product/{id}/edit', 'AdminProductController@edit')
+    ->name('admin_productEdit');
+Route::post('/admin/product/{id}/edited', 'AdminProductController@update')
+    ->name('admin_productEdited');
+Route::get('/admin/product/{id}/delete', 'AdminProductController@destroy')
+    ->name('admin_productDestroy');
