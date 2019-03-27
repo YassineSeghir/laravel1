df73725257ff9cd14133144c5e018660e16240e9
Richard Weber <richard.weber@Digital-Grenoble.local>
2019-03-27 17:40:11 +0100

    navigation admin logged guest

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,29 +6,31 @@
     <title>@yield('title')</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Merriweather|Pacifico" rel="stylesheet">
-    <!--styles-->
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
     <link href="{{ asset ('css/app.css')}}" rel="stylesheet">
     <link href="{{ asset ('css/style.css')}}" rel="stylesheet">
-    <!--Font awesome-->
+    <!-- Font awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
           integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP"
           crossorigin="anonymous">
 </head>
 <body>
 <div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links" >
-            @auth
-                <a href="{{ url('welcome') }}"><span class="noire>"welcome</span></a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
+    {{--@if (Route::has('login'))--}}
+        {{--<div class="top-right links" >--}}
+            {{--@auth--}}
+                {{--<a href="{{ url('welcome') }}"><span class="noire>"welcome</span></a>--}}
+            {{--@else--}}
+                {{--<a href="{{ route('login') }}">Login</a>--}}
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
-        </div>
-    @endif
+                {{--@if (Route::has('register'))--}}
+                    {{--<a href="{{ route('register') }}">Register</a>--}}
+                {{--@endif--}}
+            {{--@endauth--}}
+        {{--</div>--}}
+    {{--@endif--}}
 </div>
 <nav>
     @include('includes.navbar')
