4dad554da5befade40459dd07a94387b57fcbeac
Richard Weber <richard.weber@Digital-Grenoble.local>
2019-03-28 14:20:46 +0100

    test Cart

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -10,15 +10,24 @@ class BasketController extends Controller
 {
     public function showPanier(Request $request)
     {
-        $data = $request->session()->get('key');
-        $total = 0;
-        if ($data)
-        foreach ($data as $product) {
-
-            $total += ($product->price / 100);
+        $totalPrice = 0;
+        if ($request->session()->has('key')) {
+            $products = $request->session()->get('key');
+            foreach ($request->session()->get('key') as $product) {
+                $totalPrice += ($product->price);
+            }
         }
-        return view('panier', ['data' => $data], ['total' => $total]);
 
+        return view('panier', ['products' => $products], ['totalPrice' => $totalPrice]);
+//        $data = $request->session()->get('key');
+//        $total = 0;
+//        if ($data) {
+//            foreach ($data as $product) {
+//                $total += ($product->price / 100);
+//            }
+//        }
+//
+//        return view('panier', ['data' => $data], ['total' => $total]);
     }
 
     public function addPanier(Request $request)
@@ -30,28 +39,25 @@ class BasketController extends Controller
 
         $total = 0;
         foreach ($data as $product) {
-
             $total += ($product->price / 100);
         }
-
 
         return view('panier', ['data' => $data], ['total' => $total]);
     }
 
     public function emptyPanier(Request $request)
     {
-
         $request->session()->flush();
 
         return redirect()->route('basketFree');
     }
 
 
-    public function destroyPanier(Request $request,$id)
+    public function destroyPanier(Request $request, $id)
     {
-        $request->session()->forget('key.'.$id);
+        $request->session()->forget('key.' . $id);
 
-       return redirect()->route('basket' );
+        return redirect()->route('basket');
     }
 
 }
